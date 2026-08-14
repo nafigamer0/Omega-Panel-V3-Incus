c6c86b14c2f8204f01ca42027421dc7773c716d9
NafiGamer <notyou6934420@gmail.com>
2026-08-14 18:02:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Encoded By Py-Fuscate
-# https://github.com/Sl-Sanda-Ru/Py-Fuscate
 # Make Sure You're Running The Program With python3.10 Otherwise It May Crash
 # To Check Your Python Version Run "python -V" Command
 try:
